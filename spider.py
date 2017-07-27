88c81869b459f99e562b3fb8dd6c16c7d859a783
suxiaojin <suxiaojin928380@gmail.com>
2017-07-27 23:51:42 +0800

    727

--- spider.py
+++ spider.py
@@ -12,16 +12,31 @@ def get_one_page(url):
         return None
 
 def parse_one_page(html):
-    pattern=re.compile
+    pattern= re.compile('<dd>.*?boadr-index.*?(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                       +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                        +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>',re.S)
+    items= re.findall(pattern,html)
+    for item in items:
+        yield {
+            'index':item[0],
+            'immage':item[1],
+            'title':item[2],
+            'actor':item[3].string()[3:],
+            'time':item[4].strip()[5:],
+            'score':item[5]+item[6]
+        }
 
-    items=re.findall(pattern,html)
-    print(items)
+def write_to_file(connect):
+    with open('result.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(connect,ensure_ascii=False)+'\n')
+        f.close()
 
-def main():
+def main(offset):
     url='http://maoyan.com/board/4?'
     html=get_one_page(url)
-    print(html)
-    parse_one_page(html)
+    for item in parse_one_page(html):
+        print(item)
+        write_to__file(item)
 
 if __name__=='__main__':
     main()
