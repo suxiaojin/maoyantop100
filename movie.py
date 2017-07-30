9aa2455ef22c14eeadb7538dea04ca82079c2cb7
suxiaojin <suxiaojin928380@gmail.com>
2017-07-30 13:41:31 +0800

    730

--- movie.py
+++ movie.py
@@ -2,8 +2,8 @@ import requests
 from requests.exceptions import RequestException
 import re
 import json
-
-def get_one_page(url):
+from multiprocessing import Pool
+def get_one_pag(url):
     try:
         response=requests.get(url)
         if response.status_code==200:
@@ -13,26 +13,29 @@ def get_one_page(url):
         return None
 
 def parser_one_page(html):
-    pattern= re.compile('<dd>.*?<img data-src="(.*?)"/>.*?data-val.*?>(.*?)</a>.*?stonefont">(.*?)</span>(.*?)</div>',re.S)
-    items= re.findall(pattern,html)
+    pattern = re.compile('<img data-src="(.*?)" />.*?data-val.*?>(.*?)</a>.*?stonefont">(.*?)</span>(.*?)</div>', re.S)
+    items=re.findall(pattern,html)
     for item in items:
         yield {
-            'image':item[0],
+            'url':item[0],
             'title':item[1],
-            'like':item[2]+item[3]
+            'like':item[2]
         }
-
 def write_to_file(item):
-    with open('result11.txt','a',encoding='utf-8') as f:
+    with open('results1.txt','a',encoding='utf-8') as f:
         f.write(json.dumps(item,ensure_ascii=False)+'\n')
         f.close()
-def main():
-    url='http://maoyan.com/films?showType=2'
-    html=get_one_page(url)
+
+def main(offset):
+    url='http://maoyan.com/films?showType=2&offset=' + str(offset)
+    html=get_one_pag(url)
     for item in parser_one_page(html):
         print(item)
         write_to_file(item)
 
 
 if __name__=='__main__':
-    main()
+#    for i in range(9):
+#        main(i*30)
+    pool=Pool()
+    pool.map(main,[i*10 for i in range(9)])
